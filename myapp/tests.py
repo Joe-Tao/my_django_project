9748bbb32fd1b6924b154337f1f1b389061a53db
Joe-Tao <btbutl@163.com>
2025-04-30 17:44:05 +0200

    Initial Django project with SQLite and CI

---
+++ myapp/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+import pytest
+from django.urls import reverse
+
+@pytest.mark.django_db
+def test_homepage(client):
+    response = client.get("/")
+    assert response.status_code == 200
