16bb3ec71ba5f2b56b916e235516fc67b54ca0f3
Joe-Tao <btbutl@163.com>
2025-04-30 18:10:30 +0200

    test 2

--- myapp/tests.py
+++ myapp/tests.py
@@ -6,5 +6,5 @@ from django.urls import reverse
 
 @pytest.mark.django_db
 def test_homepage(client):
-    response = client.get("/")
+    response = client.get(reverse("home"))
     assert response.status_code == 200
